cb50832116457a333b8ff9b90dc518d3fcd4eb0d
Tucker Steil <tuckersteil@gmail.com>
2022-04-12 15:00:33 -0400

    commit tree 5d56d5b4183ef5cec4e05f341175c02017665ff9

--- index.js
+++ index.js
@@ -1,1 +1,7 @@
 // Write your code here!
+let main1 = document.querySelector('#main')
+main1.remove();
+
+let newHeader = document.createElement('H1')
+newHeader.id = "victory"
+newHeader.textContent = "Tucker is the champion"
